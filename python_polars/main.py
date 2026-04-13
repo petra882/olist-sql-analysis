85807dce16092f584391fe6fe64c845db82005f6
Артеменко Пётр <petrart2008@gmail.com>
2026-04-13 21:30:34 +0500

    Add Polars implementaation and reorganize folders

---
+++ python_polars/main.py
@@ -0,0 +1,92 @@
+import polars as pl
+import datetime as dt
+
+"""
+Olist Brazilian E-Commerce: Polars Analysis
+============================================
+Перенос SQL-запросов из projsql.sql на Polars.
+Автор: Артеменко Пётр
+Дата: 13.04.2026
+"""
+
+
+"""
+-- Помесячная динамика заказов:
+orders_dataset = pl.read_csv('olist_orders_dataset.csv', try_parse_dates=True)
+
+result = orders_dataset.select(
+    pl.col("order_purchase_timestamp").dt.month().alias("month"),
+    pl.col("order_id").alias("count")
+).group_by("month").agg(pl.col("count").count()).sort("month")
+"""
+
+"""
+-- Топ-10 категорий по объему продаж
+products_ds = pl.read_csv('olist_products_dataset.csv', try_parse_dates=True)
+order_items_ds = pl.read_csv('olist_order_items_dataset.csv', try_parse_dates=True)
+total_ds = products_ds.join(order_items_ds, on='product_id')
+result = total_ds.select(
+    pl.col("product_category_name"),
+    pl.col("price"),
+).group_by("product_category_name").agg(pl.col("price").alias("avenue").sum()).sort("avenue", descending=True).limit(10)
+"""
+
+"""
+-- Города с высокой оценкой
+customers_ds = pl.read_csv('olist_customers_dataset.csv', try_parse_dates=True)
+orders_ds = pl.read_csv('olist_orders_dataset.csv', try_parse_dates=True)
+order_review_ds = pl.read_csv('olist_order_reviews_dataset.csv', try_parse_dates=True)
+total_ds = customers_ds.join(orders_ds, on='customer_id').join(order_review_ds, on='order_id')
+result = (total_ds.select(
+    pl.col("customer_city"),
+    pl.col("review_score")
+).
+          filter(pl.col("review_score") == 5).group_by("customer_city").agg(pl.col("customer_city").count().alias("count_5")).
+          sort("count_5", descending=True).limit(5))
+"""
+
+"""
+--среднее время доставки по штатам
+orders_ds = pl.read_csv('olist_orders_dataset.csv', try_parse_dates=True)
+customers_ds = pl.read_csv("olist_customers_dataset.csv", try_parse_dates=True)
+total_ds = orders_ds.join(customers_ds, on='customer_id')
+result = (total_ds.select(
+    pl.col("customer_state"),
+    pl.col("order_status"),
+    (pl.col("order_delivered_customer_date") - pl.col("order_approved_at")).alias("det"),
+    pl.col("order_approved_at"),
+    pl.col("order_delivered_customer_date")
+).filter(pl.col("order_status") == "delivered", pl.col("order_approved_at").is_not_null(),
+         pl.col("order_delivered_customer_date").is_not_null()).
+          group_by("customer_state").agg(pl.col("det").mean().alias("avg"))).sort("avg", descending=True)
+"""
+
+"""
+--клиенты совершившие только один заказ
+order_ds = pl.read_csv('olist_orders_dataset.csv', try_parse_dates=True)
+customers_ds = pl.read_csv("olist_customers_dataset.csv", try_parse_dates=True)
+total_ds = customers_ds.join(order_ds, on='customer_id')
+result = total_ds.select(
+    pl.col("customer_unique_id").alias("customer"),
+    pl.col("order_id")
+).group_by(pl.col("customer")).agg(pl.col("order_id").count().alias("count")).filter(pl.col("count")==1)
+"""
+
+"""
+--быстрешие доставки (исключена аномалия с отрицательным временем доставки)
+orders_ds = pl.read_csv('olist_orders_dataset.csv', try_parse_dates=True)
+customers_ds = pl.read_csv("olist_customers_dataset.csv", try_parse_dates=True)
+order_items_ds = pl.read_csv('olist_order_items_dataset.csv', try_parse_dates=True)
+sellers_ds = pl.read_csv('olist_sellers_dataset.csv', try_parse_dates=True)
+total_ds = orders_ds.join(customers_ds, on='customer_id').join(order_items_ds, on='order_id').join(sellers_ds, on='seller_id')
+result = (total_ds.select(
+    pl.col("customer_city"),
+    pl.col("seller_state"),
+    pl.col("order_id"),
+    pl.col("order_status"),
+    (pl.col("order_delivered_customer_date") - pl.col("order_approved_at")).alias("det"),
+    pl.col("order_approved_at"),
+    pl.col("order_delivered_customer_date")
+).filter(pl.col("order_status") == "delivered", pl.col("order_approved_at").is_not_null(),
+         pl.col("order_delivered_customer_date").is_not_null(), pl.col("det")>0).sort("det").limit(10))
+"""
---
+++ sql/projsql.sql
@@ -0,0 +1,50 @@
+-- Помесячная динамика заказов:
+SELECT strftime('%m', order_purchase_timestamp) AS month, COUNT(order_id) as count FROM olist_orders_dataset GROUP BY month;
+-- Топ-10 категорий по объему продаж
+SELECT product_category_name, SUM(price) AS revenue FROM olist_products_dataset
+INNER JOIN olist_order_items_dataset ON olist_products_dataset.product_id = olist_order_items_dataset.product_id GROUP BY product_category_name ORDER BY revenue DESC
+LIMIT 10;
+-- География клиентов с высокой оценкой:
+SELECT customer_city, COUNT(customer_city) as count_5 FROM olist_customers_dataset INNER JOIN olist_orders_dataset ON olist_customers_dataset.customer_id = olist_orders_dataset.customer_id
+INNER JOIN olist_order_reviews_dataset ON olist_orders_dataset.order_id = olist_order_reviews_dataset.order_id WHERE review_score = 5 GROUP BY customer_city ORDER BY count_5 DESC LIMIT 5;
+-- Среднее время доставки по штатам
+SELECT 
+    c.customer_state,
+    ROUND(AVG(JULIANDAY(o.order_delivered_customer_date) - JULIANDAY(o.order_approved_at)), 1) AS avg_delivery_days
+FROM olist_orders_dataset o
+INNER JOIN olist_customers_dataset c ON o.customer_id = c.customer_id
+WHERE o.order_status = 'delivered' 
+  AND o.order_approved_at IS NOT NULL 
+  AND o.order_delivered_customer_date IS NOT NULL
+GROUP BY c.customer_state
+ORDER BY avg_delivery_days DESC;
+-- Клиенты без повторных покупок:
+SELECT customer_unique_id AS customer FROM olist_customers_dataset INNER JOIN olist_orders_dataset ON 
+olist_customers_dataset.customer_id = olist_orders_dataset.customer_id GROUP BY customer_unique_id 
+HAVING COUNT(order_id) = 1;
+-- Анализ самых быстрых доставок
+SELECT (julianday(order_delivered_customer_date) - julianday(order_approved_at)) as speed, order_id, customer_city, seller_state FROM olist_orders_dataset 
+INNER JOIN olist_order_items_dataset ON olist_orders_dataset.order_id = olist_order_items_dataset.order_id 
+INNER JOIN olist_sellers_dataset ON olist_order_items_dataset.seller_id = olist_sellers_dataset.seller_id 
+INNER JOIN olist_customers_dataset ON olist_orders_dataset.customer_id = olist_customers_dataset.customer_id 
+WHERE order_approved_at IS NOT NULL AND order_delivered_customer_date IS NOT NULL AND order_status = "delivered"
+ORDER BY speed ASC LIMIT 10;
+SELECT product_category_name
+FROM olist_products_dataset 
+INNER JOIN olist_order_items_dataset ON olist_products_dataset.product_id = olist_order_items_dataset.product_id
+INNER JOIN olist_order_payments_dataset ON olist_order_items_dataset.order_id = olist_order_payments_dataset.order_id 
+WHERE olist_order_items_dataset.order_id NOT IN (
+    SELECT order_id FROM olist_order_payments_dataset 
+    WHERE payment_type = "credit card" AND order_id IS NOT NULL
+);
+SELECT DISTINCT seller_state, AVG(payment_value) OVER(PARTITION BY seller_state) AS avg_payment_in_state FROM olist_sellers_dataset 
+INNER JOIN olist_order_items_dataset ON olist_sellers_dataset.seller_id = olist_order_items_dataset.seller_id 
+INNER JOIN olist_order_payments_dataset ON olist_order_items_dataset.order_id = olist_order_payments_dataset.order_id;
+SELECT ROW_NUMBER() OVER(PARTITION BY seller_state ORDER BY SUM(payment_value) DESC) as state_rank, seller_state, olist_order_items_dataset.seller_id, SUM(payment_value) as total_revenue
+FROM olist_sellers_dataset INNER JOIN olist_order_items_dataset ON olist_sellers_dataset.seller_id = olist_order_items_dataset.seller_id 
+INNER JOIN olist_order_payments_dataset ON olist_order_items_dataset.order_id = olist_order_payments_dataset.order_id GROUP BY olist_order_items_dataset.seller_id , seller_state 
+ORDER BY seller_state;
+SELECT seller_id, STRFTIME("%m", order_approved_at) AS month, COUNT(olist_orders_dataset.order_id) as order_count, 
+AVG(SUM(payment_value)) OVER(PARTITION BY seller_id ORDER BY STRFTIME("%m", order_approved_at) ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS moving_avg_3m 
+FROM olist_orders_dataset INNER JOIN olist_order_items_dataset ON olist_orders_dataset.order_id = olist_order_items_dataset.order_id
+INNER JOIN olist_order_payments_dataset ON olist_orders_dataset.order_id = olist_order_payments_dataset.order_id GROUP BY seller_id, month ORDER BY month
