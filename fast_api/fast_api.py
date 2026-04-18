4a813b23f61b77be0076b2da2ee8daacdee08fa4
Артеменко Пётр <petrart2008@gmail.com>
2026-04-18 21:00:40 +0500

    Upgrade ML model to RandomForest (acc 63.7%), add encoder, update FastAPI and Docker

--- fast_api/fast_api.py
+++ fast_api/fast_api.py
@@ -1,14 +1,18 @@
+import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 
-
-review_model = joblib.load("fast_api/review_model.pkl")
+encoder = joblib.load("fast_api/encoder.pkl")
+review_model = joblib.load("fast_api/review_model_v2.pkl")
 
 class PredictValue(BaseModel):
     price: float
     freight_value: float
     payment_value: float
+    delivery_time: float
+    product_category_name: str
+    seller_state: str
 
 app = FastAPI()
 @app.get("/")
@@ -16,6 +20,10 @@ async def root():
     return {"message": "Hello world"}
 @app.post("/predict")
 async def predict(request: PredictValue):
-    features = [[request.price, request.freight_value, request.payment_value]]
+    numeric_features = [[request.price, request.freight_value, request.payment_value,
+                 request.delivery_time]]
+    str_features = [[request.product_category_name, request.seller_state]]
+    str_features_encoded = encoder.transform(str_features)
+    features = np.hstack([numeric_features, str_features_encoded])
     predict = review_model.predict(features)
     return{"predicted_score": int(predict[0])}
--- python_polars/ml_model.py
+++ python_polars/ml_model.py
@@ -1,7 +1,8 @@
 import polars as pl
 import joblib
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
@@ -11,16 +12,40 @@ from sklearn.metrics import accuracy_score
 order_reviews_ds = pl.read_csv("olist_order_reviews_dataset.csv", try_parse_dates=True)
 order_items_ds = pl.read_csv("olist_order_items_dataset.csv", try_parse_dates=True)
 order_payments_ds = pl.read_csv("olist_order_payments_dataset.csv", try_parse_dates=True)
-total_ds = order_reviews_ds.join(order_items_ds, on = "order_id").join(order_payments_ds, on = "order_id")
+orders_ds = pl.read_csv("olist_orders_dataset.csv", try_parse_dates=True)
+sellers_ds = pl.read_csv("olist_sellers_dataset.csv", try_parse_dates=True)
+products_ds = pl.read_csv("olist_products_dataset.csv")
+total_ds = (order_reviews_ds.join(order_items_ds, on = "order_id").join(order_payments_ds, on = "order_id")
+            .join(orders_ds, on = "order_id")).join(sellers_ds, on = "seller_id").join(products_ds, on = "product_id")
 result = total_ds.select(
     pl.col("price"),
     pl.col("freight_value"),
     pl.col("payment_value"),
-    pl.col("review_score")
+    pl.col("review_score"),
+    (pl.col("order_delivered_customer_date") -pl.col("order_purchase_timestamp")).dt.total_days().alias("delivery_time"),
+    pl.col("product_category_name"),
+    pl.col("seller_state")
 ).drop_nulls()
-X = result.drop("review_score").to_numpy()
+result_to_encode = result.select(
+    pl.col("product_category_name"),
+    pl.col("seller_state")
+).to_numpy()
+encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+
+X_categorical_encoded = encoder.fit_transform(result_to_encode)
+
+X_numeric = result.drop(["review_score", "product_category_name", "seller_state"]).to_numpy()
+
 y = result["review_score"].to_numpy()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-model = LogisticRegression(max_iter = 500)
+
+X_final = np.hstack([X_numeric, X_categorical_encoded])
+
+X_train, X_test, y_train, y_test = train_test_split(X_final, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier()
 model.fit(X_train, y_train)
-joblib.dump(model, "review_model.pkl")
+y_pred = model.predict(X_test)
+print(accuracy_score(y_test, y_pred))
+
+joblib.dump(encoder, "D:/projects/e-commercial/fast_api/encoder.pkl")
+joblib.dump(model, "D:/projects/e-commercial/fast_api/review_model_v2.pkl")
